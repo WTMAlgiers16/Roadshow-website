1a813a60e98b860e0f369031662c2c73ea7f801e
Meriem1551 <mm_boussaid@esi.dz>
2025-08-23 15:25:32 +0100

    adding footer

---
+++ components/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer(){
+    return(
+        <div className="text-white flex justify-between md:text-base text-xs">
+            <p>©2025  All rights reserved</p>
+            <p>Made with 💚 by WTM Algiers</p>
+        </div>
+    )
+}
