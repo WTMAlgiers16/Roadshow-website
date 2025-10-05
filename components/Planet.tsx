7242c684d0174afc702272595f02e51c54c26128
Meriem1551 <mm_boussaid@esi.dz>
2025-10-05 21:06:26 +0100

    I fixed the responsive

--- components/Planet.tsx
+++ components/Planet.tsx
@@ -12,8 +12,8 @@ const Planet = ({ image, status, name, universityImage }: PlanetProps) => {
   const renderDialog = () => {
     if (status === "upcoming") {
       return (
-        <div className="absolute -top-10 flex flex-col items-center">
-          <div className="relative bg-white text-black text-sm px-4 py-2 rounded-lg">
+        <div className="absolute -top-[40px]  md:-top-[20px] flex flex-col items-center">
+          <div className="relative bg-white text-black text-sm px-4 py-2 rounded-lg shadow-md">
             ???
           </div>
         </div>
@@ -22,20 +22,20 @@ const Planet = ({ image, status, name, universityImage }: PlanetProps) => {
 
     if (status === "ongoing") {
       return (
-        <div className="absolute -top-20 flex flex-col items-center">
-          <div className="relative bg-white text-black font-semibold text-sm px-2 py-2 rounded-lg">
+        <div className="absolute -top-[80px] flex flex-col items-center">
+          <div className="relative bg-white text-black font-semibold text-sm px-3 py-2 rounded-lg shadow-md">
             We&apos;re at {name}!
           </div>
         </div>
       );
     }
 
-    return null; // finished → no dialog
+    return null;
   };
 
   return (
     <div className="relative flex items-center justify-center">
-      {/* Dialog */}
+      {/* Dialog above the planet */}
       {renderDialog()}
 
       {/* Planet */}
@@ -46,7 +46,7 @@ const Planet = ({ image, status, name, universityImage }: PlanetProps) => {
             alt="planet"
             width={160}
             height={160}
-            className={`${status === "upcoming" ? "brightness-50" : ""} hover:cursor-pointer`}
+            className={`${status === "upcoming" ? "brightness-50" : ""} hover:cursor-pointer w-[50px] h-[50px] md:w-[140px] md:h-[140px]`}
           />
         </HoverCardTrigger>
         <HoverCardContent className="grid gap-2 w-48 text-center bg-white text-black p-4 rounded-lg shadow-lg">
@@ -63,11 +63,9 @@ const Planet = ({ image, status, name, universityImage }: PlanetProps) => {
                   className="object-contain"
                 />
               </div>
-              <div>
-                <p className="text-base text-gray-700font-semibold">
-                  {status === "ongoing" ? "Ongoing now" : "Visited"}
-                </p>
-              </div>
+              <p className="text-base text-gray-700 font-semibold">
+                {status === "ongoing" ? "Ongoing now" : "Visited"}
+              </p>
             </>
           )}
         </HoverCardContent>
